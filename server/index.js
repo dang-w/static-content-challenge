44e187cdf271057f29ba6b87a97faef4b34614cc
Dan Walsh <dgwalsh1@hotmail.com>
2022-07-15 08:40:44 +0100

    Templating in place

--- server/index.js
+++ server/index.js
@@ -2,14 +2,22 @@ const fs = require('fs');
 const path = require('path');
 const glob = require('glob');
 const express = require('express');
+// const fileUpload = require('express-fileupload');
 const marked = require('marked');
+const nunjucks = require('nunjucks');
 
 const PORT = process.env.PORT || 3001;
 const contentPath = path.resolve(__dirname, '../content');
-const parsedContentPath = path.resolve(__dirname, '../parsed-content');
+const parsedContentPath = path.resolve(__dirname, '../views/parsed-content');
 let contentUrls = [];
 
 const app = express();
+app.set('views', path.join(__dirname, 'views'));
+nunjucks.configure('views', {
+  express: app,
+  autoescape: true
+});
+app.set('view engine', 'html');
 
 // Checks that the 'content' directory exists and is accessible in the app
 fs.access(contentPath, fs.constants.F_OK, (err) => {
@@ -39,7 +47,10 @@ getContentFiles(contentPath, (err, res) => {
       const urlPath = path.dirname(pathString).replace(`${contentPath}`, '');
       contentUrls.push(urlPath);
 
-      // comment below this point
+
+
+
+      // comment on below
       const fileContents = fs.readFileSync(pathString, 'utf-8');
       const fileWithMarkdown = marked.parse(fileContents);
       const newPath = `${parsedContentPath}${urlPath}`;
@@ -47,7 +58,7 @@ getContentFiles(contentPath, (err, res) => {
       fs.mkdir(newPath, { recursive: true }, (err) => {
         if (err) throw err;
 
-        fs.writeFile(`${newPath}/index.html`, fileWithMarkdown, (err) => {
+        fs.writeFile(`${newPath}/content.html`, fileWithMarkdown, (err) => {
           if (err) throw err;
           console.log('The file has been saved!');
         });
@@ -56,10 +67,46 @@ getContentFiles(contentPath, (err, res) => {
   });
 });
 
+
+// app.use(fileUpload());
+
+// app.get('/', (req, res) => {
+//   res.render('template.html', { content: });
+// });
+
+// app.post('/upload', function(req, res) {
+//   if (!req.files || Object.keys(req.files).length === 0) {
+//     return res.status(400).send('No files were uploaded.');
+//   }
+
+//   console.log('req.files:', req.files);
+
+//   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+//   // let sampleFile = req.files.sampleFile;
+
+//   // Use the mv() method to place the file somewhere on your server
+//   // sampleFile.mv('/somewhere/on/your/server/filename.jpg', function(err) {
+//   //   if (err)
+//   //     return res.status(500).send(err);
+
+//   //   res.send('File uploaded!');
+//   // });
+// });
+
 app.use(contentUrls, (req, res, next) => {
   const { originalUrl = '' } = req;
-  const resFilePath = path.join(parsedContentPath, originalUrl, 'index.html');
-  res.sendFile(resFilePath);
+  const resFilePath = path.join(parsedContentPath, originalUrl, 'content.html');
+  const content = fs.readFileSync(resFilePath, 'utf8');
+
+  res.render('template.html', { content }, (err, html) => {
+    if (err) {
+      res.writeHead(404);
+      res.write('page not found - sorry about that :(');
+      throw err;
+    }
+
+    res.send(html);
+  });
 });
 
 app.get('*', (req, res) => {
